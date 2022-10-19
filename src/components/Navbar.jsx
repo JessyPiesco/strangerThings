fb1281da0b9ec38a869c030d1d34b55031181466
Jessy <japarker0421@gmail.com>
2022-10-19 11:03:37 -0600

    adding links/notworking yet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Router } from "react-router-dom";
 
 const Navbar = () => {
+  function handleClick(event){
+    <Router>
+    <Switch>
+        <Route exact path="/home" component={Home} />
+    </Switch>
+</Router>
+
+  }
   return (
     <div id="navbar">
       <h2> Stranger Things, Buy and Sell</h2>
-      
+
       <div id="navStuff">
-        <div className="home" href="#" onClick={this.handleClick}> Home </div>
-        <div className="posts" href="#" onClick={this.handleClick}> Posts </div>
-        <div id="login" href="#" onClick={this.handleClick}> Login </div>
+        <div className="home" onClick={<a href="http://localhost:3000/home"/> }> Home </div>
+        <div className="posts" onClick={<a href="http://localhost:3000/posts"/> }> Posts </div>
+        <div id="login" onClick={<a href="http://localhost:3000/login"/> }> Login </div>
       </div>
     </div>
   );
