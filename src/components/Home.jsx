39307d823e15fb20cbb92a33532df69c704b6239
Jessy <japarker0421@gmail.com>
2022-10-23 13:48:46 -0600

    messages half working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import Login from "./Login";
 
 const Home = (props) => {
-  const username = props.username;
+  const loggedInUser = props.loggedInUser;
+  const yourMessages = props.loggedInUser.messages
+  const username = props.loggedInUser.username
 
   return (
     <div id="Home">
-      {/* {console.log(loggedInUser)} */}
       <h2> {`Welcome, ${username}`}</h2>
+      <div>{console.log(loggedInUser)}
+      {console.log(yourMessages)}
+        <h3>Received Messages </h3>
+        { yourMessages ?
+        yourMessages.map((msg) => {
+           if (msg.fromUser.username != username ) return (
+           <div key={`message${msg._id}`}>
+            <div>Message- {msg.content}</div>
+            <div>From- {msg.fromUser.username}</div>
+            <div></div>
+            </div>
+            );
+          }):
+
+          <h2>No Messages Currently</h2>}</div>
     </div>
   );
 };
